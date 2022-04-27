6084e0177f164d85c395491c1bc38ad470cee9b1
ClaraPapaya <clarawiegand@gmail.com>
2022-04-27 11:40:25 +0200

    Add Reject button as MenuButton in RequestCard

--- components/RequestsList/RequestCard.js
+++ components/RequestsList/RequestCard.js
@@ -1,7 +1,8 @@
 import { Avatar } from "@chakra-ui/avatar";
 import { Button } from "@chakra-ui/button";
 import { Heading, Link, Stack, Text } from "@chakra-ui/layout";
-import { Select } from '@chakra-ui/react'
+import { MenuButton, Menu, MenuList, MenuItem } from '@chakra-ui/react'
+import { ChevronDownIcon } from '@chakra-ui/icons'
 import ReactMarkdown from "react-markdown";
 import ChakraUIRenderer from "chakra-ui-markdown-renderer";
 import Icon from "@chakra-ui/icon";
@@ -76,21 +77,6 @@ const RequestCard = ({ request }) => {
           </ReactMarkdown>
         </Stack>
 
-        <Stack
-          direction={"column"}
-          textAlign={"right"}
-          justifyContent={"center"}
-        >
-          {/* set a label "status", make some status read-only */}
-          <Select placeholder="Pending" variant="filled" >
-            <option value="option1">Mentorship started</option>
-            <option value="option2">Mentorship finished</option>
-            <option value="option3">Not a good fit</option>
-            <option value="option4">Request expired</option>
-            <option value="option5">Mentor busy</option>
-          </Select>
-
-        </Stack>
 
         <Stack
           direction={"column"}
@@ -98,7 +84,15 @@ const RequestCard = ({ request }) => {
           justifyContent={"center"}
         >
           <Button colorScheme={"greenButton"}>Accept</Button>
-          <Button colorScheme={"grayButton"}>Reject</Button>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              Reject
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Not a good fit</MenuItem>
+              <MenuItem>I&apos;m busy</MenuItem>
+            </MenuList>
+          </Menu>
           <Button colorScheme={"blueButton"}>Contact</Button>
         </Stack>
       </Stack>
