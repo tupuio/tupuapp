817cc41980936832e24442c520cd0fbf878541be
sabine-mansour <sabine_mansour@icloud.com>
2022-05-16 15:41:54 +0200

    Created form

---
+++ components/Mentee-Mentor/index.js
@@ -0,0 +1,21 @@
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from '@chakra-ui/react';
+
+return (
+  <>
+    <FormControl>
+
+      <FormLabel>Goals</FormLabel>
+      <Input placeholder='Write your Goals' />
+
+      <FormLabel>Notes</FormLabel>
+      <Input placeholder='Write some Notes' />
+
+    </FormControl></>
+);
+
+export default Mentee - Mentor;
