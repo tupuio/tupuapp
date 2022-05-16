e272399ae6d5f8642c19a850ab5c58cb3f4ff193
sabine-mansour <sabine_mansour@icloud.com>
2022-05-16 15:49:22 +0200

    Added Save buttons

--- components/Mentee-Mentor/index.js
+++ components/Mentee-Mentor/index.js
@@ -1,21 +1,27 @@
 import {
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
 } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 
-return (
-  <>
-    <FormControl>
+const mentorship = () => {
+  return (
+    <>
+      <FormControl>
 
-      <FormLabel>Goals</FormLabel>
-      <Input placeholder='Write your Goals' />
+        <FormLabel>Goals</FormLabel>
+        <Input placeholder='Write your Goals' />
+        <Button colorScheme="greenButton" variant='outline'>
+          Save
+        </Button>
 
-      <FormLabel>Notes</FormLabel>
-      <Input placeholder='Write some Notes' />
+        <FormLabel>Notes</FormLabel>
+        <Input placeholder='Write some Notes' />
+        <Button colorScheme="greenButton" variant='outline'>
+          Save
+        </Button>
 
-    </FormControl></>
-);
+      </FormControl></>
+  );
+}
 
-export default Mentee - Mentor;
