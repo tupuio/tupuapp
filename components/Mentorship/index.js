c6a5f71de469a520d500971c5462d33f023bf415
sabine-mansour <sabine_mansour@icloud.com>
2022-05-16 16:03:07 +0200

    Added Textarea

--- components/Mentorship/index.js
+++ components/Mentorship/index.js
@@ -5,18 +5,19 @@ import {
 import { Button } from '@chakra-ui/react';
 
 const mentorship = () => {
+
   return (
     <>
       <FormControl>
 
         <FormLabel>Goals</FormLabel>
-        <Input placeholder='Write your Goals' />
+        <Textarea placeholder='Write your Goals' />
         <Button colorScheme="greenButton" variant='outline'>
           Save
         </Button>
 
         <FormLabel>Notes</FormLabel>
-        <Input placeholder='Write some Notes' />
+        <Textarea placeholder='Write some Notes' />
         <Button colorScheme="greenButton" variant='outline'>
           Save
         </Button>
