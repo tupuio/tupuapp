1963c689907c7f37c94a6b41cdf5c5262a799435
Lorenzo Pirritano <lorepirri@gmail.com>
2022-07-22 17:10:24 +0200

    add DB types for requests/relationships status

---
+++ types/dbTablesEnums.js
@@ -0,0 +1,11 @@
+export const RequestStatusEnum = Object.freeze({
+  Pending: "pending",
+  Rejected: "rejected",
+  Accepted: "accepted",
+  Expired: "expired"
+});
+
+export const RelationshipStatusEnum = Object.freeze({
+  Started: "started",
+  Finished: "finished"
+});
