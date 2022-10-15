fbceec00f29192764220103c8e68054e23be75fc
Tudor Golubenco <tudor@xata.io>
2022-10-15 18:12:20 +0200

    Started using the Xata SDK instead of simply fetch

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -1,22 +1,10 @@
 import { getSession } from "next-auth/react";
-import { getXataHeaders, DB_PATH } from "../../services";
+import { getXataClient } from "../../services/xata";
 import { sendPreferencesUpdatedEmail } from "../../utils/email";
 
 async function getByEmail(email) {
-  const resp = await fetch(`${DB_PATH}/tables/users/query`, {
-    method: "POST",
-    headers: {
-      ...(await getXataHeaders()),
-    },
-    body: JSON.stringify({
-      filter: {
-        email,
-      },
-    }),
-  });
-
-  const { records } = await resp.json();
-  return records.length > 0 ? records[0] : null;
+  const xata = await getXataClient();
+  return xata.db.users.filter("email", email).getFirst();
 }
 
 async function handleGET(session, req, res) {
@@ -35,41 +23,30 @@ async function handleGET(session, req, res) {
 async function handlePUT(session, req, res) {
   const profile = req.body.profile;
 
-  const data = await getByEmail(session.user.email);
-  const response = await fetch(`${DB_PATH}/tables/users/data/${data["id"]}`, {
-    method: "PUT",
-    headers: {
-      ...(await getXataHeaders()),
-    },
-    body: JSON.stringify({
-      name: profile.name,
-      email: data.email, // email is readonly, overwrite what comes from the form #57
-      title: profile.title,
-      company: profile.company,
-      seniority: profile.seniority,
-      biography: profile.biography,
-      twitter: profile.twitter,
-      linkedin: profile.linkedin,
-      languages: profile.languages,
-      timezone: profile.timezone,
-      picture: profile.picture,
-      roles: data.roles,
-      mentor: {
-        status: data.mentor?.status,
-        hide: data.mentor?.hide,
-        calendly: profile.mentor?.calendly,
-        longterm: profile.mentor?.longterm,
-        shortterm: profile.mentor?.shortterm,
-        notifications: profile.mentor?.notifications,
-        incognito: profile.mentor?.incognito,
-      },
-    }),
-  });
-  if (response.status > 299) {
-    res.status(response.status).json(await response.json());
+  const user = await getByEmail(session.user.email);
+  if (user === null) {
+    res.status(404).json({ message: "Not found" });
     return;
   }
-
+  user.update({
+    name: profile.name,
+    title: profile.title,
+    company: profile.company,
+    seniority: profile.seniority,
+    biography: profile.biography,
+    twitter: profile.twitter,
+    linkedin: profile.linkedin,
+    languages: profile.languages,
+    timezone: profile.timezone,
+    picture: profile.picture,
+    mentor: {
+      calendly: profile.mentor?.calendly,
+      longterm: profile.mentor?.longterm,
+      shortterm: profile.mentor?.shortterm,
+      notifications: profile.mentor?.notifications,
+      incognito: profile.mentor?.incognito,
+    },
+  });
   if (process.env.DEV_EMAIL_RECIPIENT) {
     const firstName = profile.name.split(" ")[0];
     sendPreferencesUpdatedEmail(profile.email, firstName);
