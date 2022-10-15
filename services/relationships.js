134f548623bf0d398f26a9379d95f7121dd492ff
Tudor Golubenco <tudor@xata.io>
2022-10-15 20:26:30 +0200

    changed more instances of fetch

--- services/relationships.js
+++ services/relationships.js
@@ -1,4 +1,4 @@
-import { getXataHeaders, DB_PATH } from "./index";
+import { getXataClient } from "./xata";
 import { RelationshipStatusEnum } from "../types/dbTablesEnums";
 
 export async function createRelationship(request, res) {
@@ -7,19 +7,9 @@ export async function createRelationship(request, res) {
     mentee: request.mentee.id,
     message: request.message,
     status: RelationshipStatusEnum.Started,
-    startDate: (new Date()).toJSON(), // UTC
+    startDate: new Date().toJSON(), // UTC
   };
-  const resp = await fetch(`${DB_PATH}/tables/relationships/data`, {
-    method: "POST",
-    headers: {
-      ...(await getXataHeaders()),
-    },
-    body: JSON.stringify(relationshipObj),
-  });
-
-  if (resp.status > 299) {
-    throw new Error(`Error creating relationship from request <${request.id}>: ${resp.status} ${await resp.text()}`);
-  }
-  const relationshipResponse = await resp.json();
-  return relationshipResponse;
+  const xata = getXataClient();
+  const relationship = await xata.db.relationships.create(relationshipObj);
+  return relationship;
 }
