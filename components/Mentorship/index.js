61e2119b3a9fbcfaee38d2727bd686227b71501d
Lorenzo Pirritano <lorepirri@gmail.com>
2022-12-11 21:18:56 +0100

    chore comment out old code

--- components/Mentorship/index.js
+++ components/Mentorship/index.js
@@ -1,61 +1,61 @@
-import {
-  FormControl,
-  FormLabel,
-} from '@chakra-ui/react';
-import { Button } from '@chakra-ui/react';
+// import {
+//   FormControl,
+//   FormLabel,
+// } from '@chakra-ui/react';
+// import { Button } from '@chakra-ui/react';
 
 
-const onSubmit = async (data) => {
-  const resp = await fetch("/api/request", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      message: data.message,
-    }),
-  });
-  if (resp.status > 299) {
-    toast({
-      title: "Error",
-      description: "Error creating request",
-      status: "error",
-      position: "top",
-      isClosable: true,
-    });
-  } else {
-    onClose();
-  }
-};
+// const onSubmit = async (data) => {
+//   const resp = await fetch("/api/request", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       message: data.message,
+//     }),
+//   });
+//   if (resp.status > 299) {
+//     toast({
+//       title: "Error",
+//       description: "Error creating request",
+//       status: "error",
+//       position: "top",
+//       isClosable: true,
+//     });
+//   } else {
+//     onClose();
+//   }
+// };
 
-const mentorship = () => {
+// const mentorship = () => {
 
-  return (
-    <>
-      <FormControl>
+//   return (
+//     <>
+//       <FormControl>
 
-        <FormLabel>Goals</FormLabel>
-        <Textarea placeholder='Write your Goals' />
-      </FormControl>
-      <FormControl>
+//         <FormLabel>Goals</FormLabel>
+//         <Textarea placeholder='Write your Goals' />
+//       </FormControl>
+//       <FormControl>
 
-        <FormLabel>Notes</FormLabel>
-        <Textarea placeholder='Write some Notes' />
-      </FormControl>
-      <FormControl>
-        <Button colorScheme="greenButton" variant='outline'>
-          Save
-        </Button>
-      </FormControl>
-      <FormControl>
-        <FormLabel>Mentorship Status</FormLabel>
-        <Select placeholder='End Mentorship?'>
-          <option>Finish</option>
-          <option>Long-term Mentorship</option>
-        </Select>
-      </FormControl>
-    </>
-  );
-}
+//         <FormLabel>Notes</FormLabel>
+//         <Textarea placeholder='Write some Notes' />
+//       </FormControl>
+//       <FormControl>
+//         <Button colorScheme="greenButton" variant='outline'>
+//           Save
+//         </Button>
+//       </FormControl>
+//       <FormControl>
+//         <FormLabel>Mentorship Status</FormLabel>
+//         <Select placeholder='End Mentorship?'>
+//           <option>Finish</option>
+//           <option>Long-term Mentorship</option>
+//         </Select>
+//       </FormControl>
+//     </>
+//   );
+// }
 
-export default mentorship;
+// export default mentorship;
