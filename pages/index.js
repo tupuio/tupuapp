219394f0e3188b1d23bbc1ba1970f484d250171c
Lorenzo Pirritano <lpirritano@anaconda.com>
2023-02-07 23:54:25 -0500

    fix swr order, clean logs #72

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,18 @@ import { useRouter } from 'next/router'
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Home({ mode }) {
+  const { data: requestsData } = useSWR("/api/requestsCount", fetcher);
+  const { data: menteesData } = useSWR("/api/menteesCount", fetcher);
+  const { data: profile } = useSWR("/api/profile", fetcher);
+  const { data: applicationsData } = useSWR("/api/applicationsCount", fetcher);
+  const { data: mentorshipsData } = useSWR("/api/mentorshipsCount", fetcher);
+
   const router = useRouter();
-  console.log('mode', mode);
   
   let homePath = '/profile';
   if (mode === "mentor") {
-    const { data: requestsData } = useSWR("/api/requestsCount", fetcher);
-    const { data: menteesData } = useSWR("/api/menteesCount", fetcher);
-    const { data: profile } = useSWR("/api/profile", fetcher);
     const requestsCount = requestsData?.count || 0;
     const menteesCount = menteesData?.count || 0;
-    console.log('requestsCount', requestsCount);
-    console.log('menteesCount', menteesCount);
-    console.log('profile.mentor', profile?.mentor);
     
     if (profile && !profile?.mentor?.calendly) {
       // will go to the profile to let the mentor set their calendar link
@@ -27,8 +26,6 @@ export default function Home({ mode }) {
       homePath = '/mentees';
     }
   } if (mode === "mentee") {
-    const { data: applicationsData } = useSWR("/api/applicationsCount", fetcher);
-    const { data: mentorshipsData } = useSWR("/api/mentorshipsCount", fetcher);
     const applicationsCount = applicationsData?.count || 0;
     const mentorshipsCount = mentorshipsData?.count || 0;
     if (mentorshipsCount) {
@@ -39,8 +36,6 @@ export default function Home({ mode }) {
       homePath = '/mentors';
     }
   }
-  console.log('path:', homePath);
-  // go to profile
   router.push(homePath);
   return <></>;
 }
