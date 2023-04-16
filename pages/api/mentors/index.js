6d2c4ec6496c6c68d39e3ac0621d4a483527f9fc
Matt <matthew.robinson212@gmail.com>
2023-04-16 17:38:07 +0100

    fix: exclude mentors for whom the user has a pending request

--- pages/api/mentors/index.js
+++ pages/api/mentors/index.js
@@ -1,6 +1,7 @@
 import { getSession } from "next-auth/react";
 import { getXataClient } from "../../../services/xata";
 import { getUser } from "../../../services";
+import { RequestStatusEnum } from "../../../types/dbTablesEnums";
 
 export default async function handler(req, res) {
   const xata = getXataClient();
@@ -12,7 +13,7 @@ export default async function handler(req, res) {
     return;
   }
 
-  const user = await getUser(session);
+  const currentUser = await getUser(session);
 
   // this does not work:
   // $includesAll: [{ $contains: "mentor" }, { $contains: "test" }],
@@ -26,10 +27,11 @@ export default async function handler(req, res) {
     },
     {
       $not: {
-        "id": user.id
+        "id": currentUser.id
       }
     }
   ];
+
   if (
     process.env.NODE_ENV === "development" ||
     process.env.NEXT_PUBLIC_DEV_LOGIN === "true"
@@ -39,11 +41,38 @@ export default async function handler(req, res) {
     allFilters.push({ $not: { roles: { $includes: "test" } } });
   }
 
+  // first get all the mentors, excluding the current user
   const mentors = await xata.db.users
     .filter({
       $all: allFilters,
     })
     .getAll();
 
-  res.status(200).json({ records: mentors });
+
+  // next get all the requests the current user has made 
+  const requests = await xata.db.requests
+    .select(["mentor.id"])
+    .filter({
+      "mentee.id": currentUser.id,
+      status: RequestStatusEnum.Pending,
+    })
+    .getAll();
+
+  // exclude any mentors for which the current user has a pending request
+  const filteredMentors = mentors.filter((mentor) => {
+
+    // iterate through the requests array
+    // if any of the request mentor IDs are contained with the mentor ID array 
+    // exclude the mentor record
+    return !requests.some((request) => {
+      const requestMentorId = request?.mentor?.id
+
+      return requestMentorId ? requestMentorId === mentor.id : false
+    })
+
+  })
+
+
+
+  res.status(200).json({ records: filteredMentors });
 }
