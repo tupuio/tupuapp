fc7a59a4f4fc18315ffa5a7df3e476f61a361bbd
Matt <matthew.robinson212@gmail.com>
2023-06-11 13:52:28 +0100

    feat: load profile page for unpublished users

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,30 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 export default function Home({ mode }) {
   const { data: requestsData } = useSWR("/api/requestsCount", fetcher);
   const { data: menteesData } = useSWR("/api/menteesCount", fetcher);
-  const { data: profile } = useSWR("/api/profile", fetcher);
+  const { data: profile, isLoading: isProfileLoading } = useSWR("/api/profile", fetcher);
   const { data: applicationsData } = useSWR("/api/applicationsCount", fetcher);
   const { data: mentorshipsData } = useSWR("/api/mentorshipsCount", fetcher);
 
   const router = useRouter();
-  
+
   let homePath = '/profile';
+
+  // wait for the profile data to load first so we can check if the user has been published
+  // before loading the first page
+  if (!profile || isProfileLoading) {
+    return <>Loading...</>
+  }
+
+  // the only page an unpublished user can access is the profile page
+  if (!profile.published) {
+    router.push(homePath);
+    return <></>;
+  }
+
   if (mode === "mentor") {
     const requestsCount = requestsData?.count || 0;
     const menteesCount = menteesData?.count || 0;
-    
+
     if (profile && !profile?.mentor?.calendly) {
       // will go to the profile to let the mentor set their calendar link
       homePath = '/preferences';
