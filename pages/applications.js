ecbcd9bc481d38e7696b823ed54a4407d45e9e21
Matt <matthew.robinson212@gmail.com>
2023-07-16 15:04:49 +0100

    feat: add protection to applications page

--- pages/applications.js
+++ pages/applications.js
@@ -7,12 +7,17 @@ export default function ApplicationsPage() {
       <Heading mb={10}>Applications</Heading>
       <Box maxWidth="4xl">
         <Text>
-          You have sent the application for mentorship to the following people. 
-          Please, be patient, they will accept or deny your application. You can 
+          You have sent the application for mentorship to the following people.
+          Please, be patient, they will accept or deny your application. You can
           cancel an application at anytime.
         </Text>
         <ApplicationsList />
       </Box>
     </>
   );
+}
+
+ApplicationsPage.auth = {
+  roles: ['mentee'],
+  publishedOnly: true
 }
