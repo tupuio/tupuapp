333303e62389f3ee90ed016aee8a30d9c23a4f3b
Matt <matthew.robinson212@gmail.com>
2023-08-03 18:07:58 +0100

    test: test auth check

--- middleware.js
+++ middleware.js
@@ -4,6 +4,8 @@ import { getToken } from 'next-auth/jwt'
 // const applicationApiPaths = ['api/profile', 'api/']
 
 export async function middleware(request) {
+
+
   // protect API routes that are not related to authentication
   if (request.nextUrl.pathname.startsWith('/api') && !request.nextUrl.pathname.startsWith('/api/auth')) {
     // first check if the user is authenticated
@@ -12,11 +14,18 @@ export async function middleware(request) {
       secret: process.env.JWT_SECRET
     })
 
+    console.log('TEST', token)
+
     // if they are not authenticated redirect to sign in page
     if (!token) {
-      const url = new URL(`/api/auth/signin`, request.url);
-      url.searchParams.set("callbackUrl", encodeURI(request.url));
-      return NextResponse.redirect(url);
+      // const url = new URL(`/api/auth/signin`, request.url);
+      // url.searchParams.set("callbackUrl", encodeURI(request.url));
+      // return NextResponse.redirect(url);
+      return new NextResponse(
+        JSON.stringify({ success: false, message: 'access denied due to token' }),
+        { status: 404, headers: { 'content-type': 'application/json' } }
+      )
+
     }
 
     // next check if they are published
