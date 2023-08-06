8dc8b120767bd505b8969c816288c1d6cfa28ff9
Matt <matthew.robinson212@gmail.com>
2023-08-06 16:38:08 +0100

    chore: create constants for roles

--- constants/index.js
+++ constants/index.js
@@ -1,1 +1,2 @@
 export * from './api-constants'
+export * from './roles-constants'
---
+++ constants/roles-constants.js
@@ -0,0 +1,4 @@
+export const ADMIN_ROLE = 'admin'
+export const MENTOR_ROLE = 'mentor'
+export const MENTEE_ROLE = 'mentee'
+export const TEST_ROLE = 'test'
