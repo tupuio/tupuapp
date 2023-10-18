6e76e04f87c955e88c1df09ff435aaa12b5bacf5
Matt <matthew.robinson212@gmail.com>
2023-10-18 08:27:26 +0100

    feat: more auth checker tests

--- components/AuthChecker/AuthChecker.js
+++ components/AuthChecker/AuthChecker.js
@@ -1,6 +1,8 @@
 import { useSession } from "next-auth/react"
 import { ADMIN_ROLE } from "../../constants"
 
+export const ACCESS_DENIED_MESSAGE = 'Access denied'
+
 function AuthChecker({ children, authObject }) {
     const { data: session } = useSession()
 
@@ -16,7 +18,7 @@ function AuthChecker({ children, authObject }) {
 
     // if the page requires the user to have a certain role and the user does not have the role deny access
     if (authObject?.roles && !authObject.roles.some(item => session?.user?.roles?.includes(item)) && !authObject.roles.includes(ADMIN_ROLE)) {
-        return <div>Access denied</div>
+        return <div data-testid>{ACCESS_DENIED_MESSAGE}</div>
     }
 
     return children
